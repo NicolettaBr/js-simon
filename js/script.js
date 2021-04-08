d0e62064a04b9379ccc3eb31663dea55736f20b0
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-04-08 11:28:59 +0200

    aggiunto array con numeri indovinati e ciclo for per pusharli ,stampa

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 //generiamo 5 numeri random
 
 var randomNumbers = [];
+console.log(randomNumbers);
 //ciclo while per inserire 5 numeri diversi e pusharli in randomNumbers
 while (randomNumbers.length < 5){
     var numeroRandom = Math.floor(Math.random() * 100) + 1; 
@@ -32,7 +33,10 @@ var countdown = setInterval(function(){
         clearInterval(countdown);
 
         //chiedo all' utente 5 numeri e li pusho in numeriUtente usando ciclo for
-        
+        //creo array vuoto in cui vengono inseriti i 5 numeri dati dall'utente
+        var numeriUtente =[];
+        console.log(numeriUtente);
+
         for (var i = 0; i < 5; i++){
             var richiestaNumero = parseInt(prompt('dammi un numero'));
             //console.log(richiestaNumero);
@@ -42,20 +46,26 @@ var countdown = setInterval(function(){
             
             }
         }
+        
+        //creo array in cui pushare numeri dati dall' utente e presenti nell' array numeri random
+        var indovinati = [];
+        for(var i = 0; i<numeriUtente.length; i++){
+            var thisNumber = numeriUtente[i];
+            console.log(thisNumber);
+
+            if(randomNumbers.includes(thisNumber)==true){
+                indovinati.push(thisNumber);
+            }
+        }
+        console.log(indovinati);
+        //stampo numeri indovinati
+        alert('Hai trovato ' + indovinati.length + ' numeri. E questi sono : ' + indovinati);
     
     }else{
         //altrimenti continuo a decrementare secondi
         secondi--;
     }
     
-}, 1000);
-
-//creo array vuoto in cui vengono inseriti i 5 numeri dati dall'utente
-var numeriUtente =[];
-console.log(numeriUtente);
+}, 30000);
 
 
-
-//programma stampa quanti e quali numeri ha individuato l'utente
-
-    
